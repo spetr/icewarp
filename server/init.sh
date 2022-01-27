c4d7d28e7a2c0f6f8a05713c193c01d209d68dc6
Stanislav Petr <petr@digitaldata.cz>
2022-01-27 18:24:02 +0100

    Remove settings.cfg from default image

--- server/init.sh
+++ server/init.sh
@@ -19,38 +19,62 @@ cd /opt/icewarp
 
 echo 'Starting IceWarp Server'
 
-test -z "$SQL_PASSWORD_FILE" || SQL_PASSWORD=$(<$SQL_PASSWORD_FILE)
+test ! -z "$SQL_PASSWORD_FILE" && SQL_PASSWORD=$(<$SQL_PASSWORD_FILE)
 
 # Detect IP addresses and DNS servers if not set with ENV
-echo -n 'Testing network connectivity ... '
-test -z "$PUBLICIP" && PUBLICIP=$(curl http://ipecho.net/plain 2>/dev/null)
-test -z "$LOCALIP" && LOCALIP=$(hostname -I)
+echo -n 'Testing network connectivity... '
+test -z "$PUBLICIP"  && PUBLICIP=$(curl http://ipecho.net/plain 2>/dev/null)
+test -z "$LOCALIP"   && LOCALIP=$(hostname -I)
 test -z "$DNSSERVER" && DNSSERVER=$(grep -i '^nameserver' /etc/resolv.conf |head -n1|cut -d ' ' -f2)
 echo 'OK'
 
-# Put default persistent content if data dir is empty
-test -d /data/archive || (echo 'Creating archive folder'; mkdir -p /data/archive)
-test -d /data/backup || (echo 'Creating backup filder'; mkdir -p /data/backup)
-test -z "$(ls -A /data/calendar 2>/dev/null))" || (echo 'Initializing calendar folder'; tar xzf calendar-default.tgz -C /data/)
-test -z "$(ls -A /data/config 2>/dev/null))" || (echo 'Initializing config folder'; tar xzf config-default.tgz -C /data/)
-test -d /data/mail || (echo "Creating mail folder"; mkdir -p /data/mail)
-test -z "$(ls -A /data/spam 2>/dev/null)" || (echo 'Initializing spam folder'; tar xzf spam-default.tgz -C /data/)
-test -d /data/status || (echo 'Creating status folder'; mkdir -p /data/status)
-test -d /data/temp || (echo 'Creating temp folder'; mkdir -p /data/temp)
-test -d /data/temp/php || (echo 'Creating php temp folder'; mkdir -p /data/temp/php)
-test -d /data/_incoming || (echo 'Creating _incoming folder'; mkdir -p /data/_incoming)
-test -d /data/_outgoing || (echo 'Creating _outgoing folder'; mkdir -p /data/_outgoing)
+# Initialize default persistent folders
+if [ ! -f '/data/config/settings.cfg' ]; then
+   echo 'Initializing IceWarp Server - first start'
+   echo -n 'Initializing config folder... '
+   tar xzf config-default.tgz -C /data/
+	/opt/icewarp/tool.sh set system c_system_storage_dir_mailpath /data/mail/
+	/opt/icewarp/tool.sh set system c_system_services_fulltext_database_path /data/yoda/
+	/opt/icewarp/tool.sh set system c_system_storage_dir_temppath /data/temp/
+   /opt/icewarp/tool.sh set system c_system_storage_dir_logpath /data/logs/
+	/opt/icewarp/tool.sh set system c_system_tools_autoarchive_path /data/archive/
+	/opt/icewarp/tool.sh set system c_system_tools_backup_db_accountsenabled 1
+	/opt/icewarp/tool.sh set system c_system_tools_backup_db_accounts '/data/backup/accounts.db;;;;7;3'
+	/opt/icewarp/tool.sh set system c_system_tools_backup_db_asenabled 1
+	/opt/icewarp/tool.sh set system c_system_tools_backup_db_as '/data/backup/antispam.db;;;;7;3'
+	/opt/icewarp/tool.sh set system c_system_tools_backup_db_gwenabled 1
+	/opt/icewarp/tool.sh set system c_system_tools_backup_db_gw '/data/backup/groupware.db;;;;7;3'
+	/opt/icewarp/tool.sh set system c_system_tools_backup_db_directorycacheenabled 1
+	/opt/icewarp/tool.sh set system c_system_tools_backup_db_directorycache '/data/backup/directorycache.db;;;;7;3'
+	/opt/icewarp/tool.sh set system c_system_adv_ext_snmpserver 1
+	/opt/icewarp/tool.sh set system c_meeting_active 1
+	/opt/icewarp/tool.sh set system c_smsservice_active 1
+	/opt/icewarp/tool.sh set system c_system_mysqldefaultcharset utf8mb4
+	/opt/icewarp/tool.sh set system c_mail_imap_idledisable 1
+	/opt/icewarp/tool.sh set system c_system_services_imap_indexstorage 1
+	/opt/icewarp/tool.sh set system c_system_sqllogtype 3
+	/opt/icewarp/tool.sh set system c_system_storage_accounts_odbcmultithread 1
+	/opt/icewarp/tool.sh set system c_system_storage_accounts_odbcmaxthreads 20
+	/opt/icewarp/tool.sh set system c_system_storage_accounts_storagemode 2
+   echo 'OK'
+fi
+test -d /data/archive   || (echo -n 'Creating archive folder... ';   mkdir -p /data/archive;   echo 'OK')
+test -d /data/backup    || (echo -n 'Creating backup filder... ';    mkdir -p /data/backup;    echo 'OK')
+test -d /data/mail      || (echo -n 'Creating mail folder... ';      mkdir -p /data/mail;      echo 'OK')
+test -d /data/status    || (echo -n 'Creating status folder... ';    mkdir -p /data/status;    echo 'OK')
+test -d /data/temp      || (echo -n 'Creating temp folder... ';      mkdir -p /data/temp;      echo 'OK')
+test -d /data/temp/php  || (echo -n 'Creating php temp folder... ';  mkdir -p /data/temp/php;  echo 'OK')
+test -d /data/_incoming || (echo -n 'Creating _incoming folder... '; mkdir -p /data/_incoming; echo 'OK')
+test -d /data/_outgoing || (echo -n 'Creating _outgoing folder... '; mkdir -p /data/_outgoing; echo 'OK')
 
-# Create tmpfs dirs
+# Create tmpfs directories
 mkdir -p /dev/shm/var
-mkdir -p /dev/shm/sessions
-
 
 # Configure IceWarp before start
-echo -n 'Configuration tasks 1/2 ... '
+echo -n 'Configuration tasks 1/2... '
 if [ -z "$REDIS_HOST" ]; then
    sed -i 's/^session\.save_handler.*/session\.save_handler = files/g' /opt/icewarp/php/php.ini 
-   sed -i 's/^session\.save_path.*/session\.save_path = \/dev/shm/sessions/g' /opt/icewarp/php/php.ini 
+   sed -i 's/^session\.save_path.*/session\.save_path = \/data\/temp\/php/g' /opt/icewarp/php/php.ini 
 else
    sed -i 's/^session\.save_handler.*/session\.save_handler = redis/g' /opt/icewarp/php/php.ini 
    sed -i "s/^session\.save_path.*/session\.save_path = tcp:\/\/${REDIS_HOST}:6379/g" /opt/icewarp/php/php.ini 
@@ -66,7 +90,7 @@ fi
 echo 'OK'
 
 # Wait for SQL server
-echo -n 'Checking SQL server connection ... '
+echo -n 'Checking SQL server connection... '
 cat <<EOT >/root/.my.cnf
 [client]
 host = ${SQL_HOST}
@@ -82,7 +106,7 @@ done
 test "$SQL_OK" != 'true' && (echo 'SQL connection error'; exit 1)
 echo 'OK'
 
-# Create SQL databases if not exists
+# Create SQL databases
 echo -n 'Checking SQL databases ... '
 mysql -N -s -e 'CREATE DATABASE IF NOT EXISTS iw_accounts DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci'
 mysql -N -s -e 'CREATE DATABASE IF NOT EXISTS iw_activesync DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci'
@@ -92,7 +116,7 @@ mysql -N -s -e 'CREATE DATABASE IF NOT EXISTS iw_groupware DEFAULT CHARACTER SET
 mysql -N -s -e 'CREATE DATABASE IF NOT EXISTS iw_webcache DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci'
 echo 'OK'
 
-# Create tables if not exists - Accounts database
+# Create tables - Accounts database
 if ! checkMySQLTableExists 'iw_accounts' 'MetaData'; then
    echo -n 'Creating tables in accounts database ... '
    ./tool.sh create tables 0 "iw_accounts;${SQL_USER};${SQL_PASSWORD};${SQL_HOST};3;2"
@@ -112,7 +136,7 @@ if ! checkMySQLTableExists 'iw_antispam' 'MetaData'; then
    fi
 fi
 
-# Create tables if not exists - Directorycache database
+# Create tables - Directorycache database
 if ! checkMySQLTableExists 'iw_dircache' 'MetaData'; then
    echo -n 'Creating tables in directory cache database ... '
    ./tool.sh create tables 4 "iw_dircache;${SQL_USER};${SQL_PASSWORD};${SQL_HOST};3;2"
